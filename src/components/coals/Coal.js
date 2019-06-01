921b589e45e0853938e680dc49a190e487c101e8
ajurasz <arkadiusz.jurasz@gmail.com>
2019-06-01 13:05:41 +0200

    Show if item is visible on main page.

--- src/components/coals/Coal.js
+++ src/components/coals/Coal.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import {Card, Divider, Icon, Popup} from 'semantic-ui-react';
 
-const Coal = ({name, price, mine, isEditable}) => (
+const Coal = ({name, price, mine, show, isEditable}) => (
     <Card fluid={true}>
         <Card.Content textAlign='center'>
             <Card.Header className='item-name'>
-                <div className=''>{name}</div>
-                {isEditable &&
-                <Popup
-                    trigger={<Icon name='edit'/>}
-                    content='Edytuj'
-                    position='top center'
-                />
-                }
+                {name}
             </Card.Header>
+            {isEditable &&
+            <React.Fragment>
+                <Divider/>
+                <Card.Header>
+                    {show ? <Icon name='eye'/> : <Icon name='eye slash'/>}
+                    <Popup
+                        trigger={<Icon name='edit' style={{ marginLeft: '1em'}}/>}
+                        content='Edytuj'
+                        position='top center'
+                    />
+                </Card.Header>
+            </React.Fragment>
+            }
             <Divider/>
             <Card.Description className='item-price'>
                 <h2>{price} PLN</h2>
